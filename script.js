798a3034b0bf86861b3388b21e6a548f5d386a55
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2025-03-05 19:36:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("product-container")
+  .addEventListener("click", function (event) {
+    if (event.target.tagName == "BUTTON") {
+      const button = event.target;
+      // console.log(
+      //   button.parentNode.parentNode.children[1].children[1].innerText
+      // );
+    }
+  });
